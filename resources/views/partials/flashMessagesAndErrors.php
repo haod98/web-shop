2abd2630cbfa4677a8f53898c465dbd5ac79ca6b
Hao <74541600+haod98@users.noreply.github.com>
2021-11-24 14:00:03 +0100

    Fix falsely displaying error/success message

--- resources/views/partials/flashMessagesAndErrors.php
+++ resources/views/partials/flashMessagesAndErrors.php
@@ -1,13 +1,17 @@
 <!-- On error -->
-<div class="error-msg-container align-center">
-    <?php foreach (\Core\Session::getAndForget('errors', []) as $error) : ?>
-        <p class="error-msg-container__text"><?php echo $error; ?></p>
-    <?php endforeach; ?>
-</div>
+<?php if (!empty(\Core\Session::get('errors'))) : ?>
+    <div class="error-msg-container align-center">
+        <?php foreach (\Core\Session::getAndForget('errors', []) as $error) : ?>
+            <p class="error-msg-container__text"><?php echo $error; ?></p>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
 
 <!-- On success -->
-<div class="success-msg-container align-center">
-    <?php foreach (\Core\Session::getAndForget('success', []) as $success) : ?>
-        <p class="success-msg-container__text"><?php echo $success; ?></p>
-    <?php endforeach; ?>
-</div>
+<?php if (!empty(\Core\Session::get('success'))) : ?>
+    <div class="success-msg-container align-center">
+        <?php foreach (\Core\Session::getAndForget('success', []) as $success) : ?>
+            <p class="success-msg-container__text"><?php echo $success; ?></p>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
