8bbf0abf88ebb07059b51ce53a040f63d261bb23
Hao <74541600+haod98@users.noreply.github.com>
2021-12-07 18:48:33 +0100

    Add cart count

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -35,11 +35,12 @@
             </a>
         </li>
         <li>
-            <a class="navbar-container__list--anchor-reset" href="<?php echo BASE_URL . "/cart" ?>">
+            <a class="navbar-container__list--anchor-reset link-reset" href="<?php echo BASE_URL . "/cart" ?>">
                 <!-- Basket icon -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="navbar-container__icon bi bi-bag" viewBox="0 0 16 16">
                     <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
                 </svg>
+                (<?php echo \App\Services\CartService::getCount(); ?>)
             </a>
         </li>
     </ul>
