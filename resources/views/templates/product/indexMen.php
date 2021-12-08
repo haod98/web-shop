38a372b36d42b84d416e42d43d0c2de72db95268
Hao <74541600+haod98@users.noreply.github.com>
2021-12-08 13:25:35 +0100

    Add edit button in men category as admin

--- resources/views/templates/product/indexMen.php
+++ resources/views/templates/product/indexMen.php
@@ -8,6 +8,9 @@
                 </a>
                 <p class="single-product__name"><?php echo $product->name ?></p>
                 <p class="single-product__price"><?php echo $product->price ?> €</p>
+                <?php if (\Core\Middlewares\AuthMiddleware::isAdmin()) : ?>
+                    <a href="<?php echo BASE_URL . "/products/$product->id/edit" ?>" class="link-reset btn--edit mt-2">Edit</a>
+                <?php endif; ?>
             </div>
         <?php endif; ?>
     <?php endforeach; ?>
