81bd7b4221cdd6659639a536f566843abe34c9e8
Hao <74541600+haod98@users.noreply.github.com>
2021-12-08 11:59:39 +0100

    Imporove navbar

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -6,10 +6,10 @@
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </button>
-            <a href="<?php echo BASE_URL ?>">Home</a>
+            <a href="<?php echo BASE_URL ?>" class="logo">Shoplaza</a>
         </li>
         <li class="navbar-container__list--hidden">
-            <a class="navbar-container__list--anchor-reset" href="<?php echo BASE_URL; ?>">Home</a>
+            <a class="navbar-container__list--anchor-reset logo" href="<?php echo BASE_URL; ?>">Shoplaza</a>
         </li>
         <li class="navbar-container__list--hidden">
             <a class="navbar-container__list--anchor-reset" href="<?php echo BASE_URL; ?>/women">Women</a>
@@ -17,6 +17,11 @@
         <li class="navbar-container__list--hidden">
             <a class="navbar-container__list--anchor-reset" href="<?php echo BASE_URL; ?>/men">Men</a>
         </li>
+        <?php if (\Core\Middlewares\AuthMiddleware::isAdmin()) : ?>
+            <li class="navbar-container__list--hidden">
+                <a class="navbar-container__list--anchor-reset" href="<?php echo BASE_URL; ?>/products">Edit Products</a>
+            </li>
+        <?php endif; ?>
     </ul>
     <ul class="list-reset navbar-container__icon-list">
         <?php if (\Core\Middlewares\AuthMiddleware::isAdmin()) : ?>
@@ -58,6 +63,11 @@
             <li>
                 <a href="<?php echo BASE_URL . "/cart" ?>">Cart</a>
             </li>
+            <?php if (\Core\Middlewares\AuthMiddleware::isAdmin()) : ?>
+                <li>
+                    <a href="<?php echo BASE_URL; ?>/products">Edit Products</a>
+                </li>
+            <?php endif; ?>
         </ul>
     </div>
 </nav>
